210ebd891db68b6213ab565c3f65c7cf507530d6
lerikoder <lerigar@gmail.com>
2015-10-18 18:51:44 -0430

    Update Plot3.R

--- Plot3.R
+++ Plot3.R
@@ -1,9 +1,10 @@
 plot3 <- function(){
         library(graphics)
         # You can setwd() according to your PC
-        with(df02, plot(newDate, Global_reactive_power / 1000, df02, main = "Global Rective Power", 
-		xlab = "Global Rective Power (kilowatts)", 
-		ylab = "Frequency", col = "red", type = "l"))
+        with(df02, plot(newDate, Sub:metering_1 / 1000, df02, main = "Sub Metering", 
+		ylab = "Energy sub metering", col = "black", type = "l"))
+	lines(newDate, Sub:metering_2 / 1000, col = "blue", lty = 1)
+	lines(newDate, Sub:metering_3 / 1000, col = "red", lty = 1)
 	dev.copy(png, file = "Plot3.png") 
 	dev.off()
 }
